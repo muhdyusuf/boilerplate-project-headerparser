504882ab9a0d7f671918c223a6f3e3f133f5ddbc
muhdyusuf <92169759+muhdyusuf@users.noreply.github.com>
2022-05-28 12:21:29 +0800

    fcc project

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
-
+const express = require('express');
+const app = express();
+const {networkInterfaces} =require("os")
+const bodyparser=require("body-parser")
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -28,6 +29,24 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+//body parser middleware
+
+// app.use(bodyparser.urlencoded({extended:false}))
+// app.use(bodyparser.json())
+
+app.enable('trust proxy')
+app.get("/api/whoami",(req,res)=>{
+  
+  res.send({
+    
+    ipaddress:req.ip,
+    language:req.headers['accept-language'],
+    software:req.header('User-Agent'),
+  })
+})
